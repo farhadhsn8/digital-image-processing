480e37ea05bbe196f265ea1976668c8ee59bfe16
farhadhsn8 <farhadhsn8@gmail.com>
2022-04-07 13:02:27 +0430

    Q5

--- تمرین1/Q5/Q5.py
+++ تمرین1/Q5/Q5.py
@@ -13,7 +13,7 @@ print(lena.shape)  # (512, 512)
 otsu_threshold, image_result = cv2.threshold(lena, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
 
 
-print("Obtained threshold: ", otsu_threshold)    # Obtained threshold:  117.0
+print("Obtained threshold: ", otsu_threshold)    #  Obtained threshold:  117.0
 plt.imshow(image_result,cmap='gray')
 plt.show()
 cv2.imwrite('otsu_threshold117.png', image_result)
